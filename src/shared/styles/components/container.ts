559df4f9ef91645ec7c08bd3bbce75ccdc04ed94
Emilia <emiliasteaa@gmail.com>
2026-09-13 13:22:01 -0300

    upd

--- src/shared/styles/components/container.ts
+++ src/shared/styles/components/container.ts
@@ -3,7 +3,7 @@ import {cva} from "class-variance-authority";
 export const containerVariants = cva(["mx-auto", "w-full"], {
   variants: {
     variant: {
-      default: "max-w-1400 px-4 md:px-6",
+      default: "max-w-1400 px-4 md:px-8",
     },
   },
   defaultVariants: {
